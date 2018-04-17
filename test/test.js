0c0355a8cdcf0b942ecffc916e7cdb8a822cd93a
lukasferreira <unlisislukasferreira@hotmail.com>
2018-04-17 16:54:12 -0300

    adding init test module

---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+require('reflect-metadata')
+var expect = require('chai').expect;
+var index = require('../dist/index.js');
+
+describe('module test', () => {
+    it('should exist all modules', () => {
+        expect(new index.IBObject()).to.exist;
+    });
+});
