e934f3e190bd588089d11a92f7cc28597aaba0db
lukasferreira <unlisislukasferreira@hotmail.com>
2018-04-17 18:39:32 -0300

    minor updates

--- test/test.js
+++ test/test.js
@@ -3,8 +3,11 @@ var reflect = require('reflect-metadata')
 var expect = require('chai').expect;
 var index = require('../dist/index.js');
 
-describe('module test', () => {
-    it('should exist all modules', () => {
+describe('module tests', () => {
+    it('should exist IBObject module', () => {
         expect(new index.IBObject()).to.exist;
+    });
+    it("should exist IBCard module", () => {
+        expect(new index.IBCard()).to.exist;
     });
 });
